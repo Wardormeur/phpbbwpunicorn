cf3c2bd9f5a00fda23f41138b8aa3930c0aaadc5
Wardormeur <wardormeur@gmail.com>
2015-10-03 11:29:12 +0200

    Fix warning on php5.5 regarding usage of namespace

--- proxy/PathFixer.php
+++ proxy/PathFixer.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+namespace wardormeur\phpbbwpunicorn;
 
 use \PHPParser_Node;
 use \PHPParser_Node_Stmt;
@@ -7,17 +8,16 @@ use \PHPParser_Node_Name;
 use \PHPParser_Node_Scalar;
 
 
-namespace wardormeur\phpbbwpunicorn;
 
 class PathFixer extends \PHPParser_NodeVisitorAbstract
 {
 	private $path_to_fix;
 	private $fixed_paths;
 	private $temp_path_string;
-	
+
 	public function __construct( $path_to_fix, array $fixed_paths){
 		$this->path_to_fix = $this->clean($path_to_fix);
-		
+
 		$this->fixed_paths = $fixed_paths;
 	}
 
@@ -26,40 +26,40 @@ class PathFixer extends \PHPParser_NodeVisitorAbstract
     public function enterNode(\PHPParser_Node $node) {
 		//We want to limit this fix to path for function that make use of paths
 		//this is achoice for my use case, be free to modify/remove it
-		if ( $node instanceof Node\Expr\Include_ ){	
+		if ( $node instanceof Node\Expr\Include_ ){
 				//We flush the string content
-				$this->temp_path_string = '';	
+				$this->temp_path_string = '';
 		}
 		if( $node instanceof Node\Expr\Variable ){
 			$this->temp_path_string = $this->temp_path_string. $node->name;
 		}
-		if( $node instanceof Node\Scalar\String_){			
+		if( $node instanceof Node\Scalar\String_){
 			$this->temp_path_string = $this->temp_path_string. $node->value;
 		}
 		return $node;
 	}
-	
+
 	public function leaveNode(\PHPParser_Node $node){
 	//on leave node, if the full string have been found, we replace its content by our new one
 		if ( $node instanceof Node\Expr\Include_ ){
-			if($this->temp_path_string == $this->path_to_fix){	
+			if($this->temp_path_string == $this->path_to_fix){
 				$node = $this->fixpath($node);
 				$this->removePath($is_path);
 				return $node;
-			}		
+			}
 		}
-	
+
 	}
-	
+
 	private function clean($path){
-		
+
 		//preg_quote reverse?
 		//trim, watfor? :D
 		$cleaned = str_replace(['$','\\\'','\\.',' '],'',$path);
-	
+
 		return $cleaned;
 	}
-	
+
 	private function fixpath($node){
 		//$s_node = new Node\Expr\Include_(;
 		for ($i=count($this->fixed_paths)-1;$i>0;$i--){
@@ -69,10 +69,10 @@ class PathFixer extends \PHPParser_NodeVisitorAbstract
 				$s_node = new Node\Expr\BinaryOp\Concat($s_node, $this->fixed_paths[count($this->fixed_paths)-$i],[]) ;
 			}
 		}
-			
+
 		return new Node\Expr\Include_($s_node,$node->type,[]);
 	}
-	
+
 	private function removePath($path_index){
 		unset($this->function[$path_index]);
 	}
--- proxy/SafeFunction.php
+++ proxy/SafeFunction.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+namespace wardormeur\phpbbwpunicorn;
 
 use \PHPParser_Node;
 use \PHPParser_Node_Stmt;
@@ -6,7 +7,6 @@ use \PHPParser_Node_Expr;
 use \PHPParser_Node_Name;
 use \PHPParser_Node_Scalar;
 
-namespace wardormeur\phpbbwpunicorn;
 
 class SafeFunction extends \PHPParser_NodeVisitorAbstract
 {
@@ -19,15 +19,15 @@ class SafeFunction extends \PHPParser_NodeVisitorAbstract
 
     public function leaveNode(\PHPParser_Node $node) {
         $is_function = array_search($node->name,$this->functions);
-		if ( $node instanceof \PHPParser_Node_Stmt_Function && $is_function!== FALSE){	
+		if ( $node instanceof \PHPParser_Node_Stmt_Function && $is_function!== FALSE){
 			$node = $this->encapsulate($node);
 			$this->removeFunction($is_function);
 			return $node;
 		}
 	}
-	
+
 	private function encapsulate($node){
-		$encapsulated_node = 
+		$encapsulated_node =
 			new \PHPParser_Node_Stmt_If(
 				new \PHPParser_Node_Expr_BooleanNot(
 					new \PHPParser_Node_Expr_FuncCall(
@@ -37,16 +37,16 @@ class SafeFunction extends \PHPParser_NodeVisitorAbstract
 						]
 					)
 				)
-				,[	
+				,[
 					'stmts' =>[$node]
 				]
 			);
-			
-			
-			
+
+
+
 		return $encapsulated_node;
 	}
-	
+
 	private function removeFunction($function_index){
 		unset($this->function[$function_index]);
 	}
